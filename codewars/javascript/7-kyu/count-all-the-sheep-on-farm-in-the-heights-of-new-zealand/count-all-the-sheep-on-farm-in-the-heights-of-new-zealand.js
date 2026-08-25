bee7e30860796b6bfac51fbed2e66d5d592f2605
Jace Rettig <94644105+SpyChicken00@users.noreply.github.com>
2026-08-25 14:38:08 +0900

    Update solution - CodeHub

--- codewars/javascript/7-kyu/count-all-the-sheep-on-farm-in-the-heights-of-new-zealand/count-all-the-sheep-on-farm-in-the-heights-of-new-zealand.js
+++ codewars/javascript/7-kyu/count-all-the-sheep-on-farm-in-the-heights-of-new-zealand/count-all-the-sheep-on-farm-in-the-heights-of-new-zealand.js
@@ -1,11 +1,16 @@
 function lostSheep(friday,saturday,total){
-  //add up friday and saturday sheep, subtract from total 
-  let sheepCount = 0;
-  //add friday sheep to count
-  //map function, for every element in the array, perform the following function with input sheep
-  friday.map((sheep) => {sheepCount += sheep})
-  //add saturday sheep to count
-  saturday.map((sheep) => {sheepCount += sheep})
-  return total - sheepCount
-​
+  //attempt 1
+//   //add up friday and saturday sheep, subtract from total 
+//   let sheepCount = 0;
+//   //add friday sheep to count
+//   //map function, for every element in the array, perform the following function with input sheep
+//   friday.map((sheep) => {sheepCount += sheep})
+//   //add saturday sheep to count
+//   saturday.map((sheep) => {sheepCount += sheep})
+//   return total - sheepCount
+  
+  //attempt 2 - use array concat and reduce functions to simplify process
+  //combine into 1 bigger array, run function on each item 
+  //total starts at total number of sheep, each iteration subtracts from the total 
+  return friday.concat(saturday).reduce((accumulatorTotal, currentValue) => accumulatorTotal - currentValue, total)
 }
