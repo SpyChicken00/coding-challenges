2869bf0e84e3d2ce8fca1b2ac9c20db93858a6db
Jace Rettig <94644105+SpyChicken00@users.noreply.github.com>
2026-08-25 13:05:59 +0900

    Add solution - CodeHub

---
+++ codewars/8-kyu/convert-a-boolean-to-a-string/convert-a-boolean-to-a-string.js
@@ -0,0 +1,7 @@
+function booleanToString(b){
+  //cast to a string literal
+  return `${b}`
+  
+  //ternary operator return true or false string depending on value
+  //return (b === true)? "true" : "false"
+}
